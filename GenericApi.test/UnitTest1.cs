ac674f8c2e1a1d8424c1d8a4ddfcdfec7e283a64
jeffrey.grassals <jeffrey.grassals@farmaciacarol.com>
2023-02-14 01:22:15 -0400

    First Commit

---
+++ GenericApi.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace GenericApi.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
